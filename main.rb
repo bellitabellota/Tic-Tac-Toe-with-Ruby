28f44de22951b875479d21c8e46f64ceef221d74
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-11 10:02:15 +0100

    Create and visualize board

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require_relative 'lib/player'
 
-
+=begin
 player1 = Player.create_player
 player1.playerinformation
 
 player2 = Player.create_player
 player2.playerinformation
+=end
 
 
+board = [[1, 2, 3],[4, 5, 6],  [7, 8, 9]]
+
+def visualize_board(board)
+  puts " #{board[0][0]} | #{board[0][1]} | #{board[0][2]} "
+  puts "-----------"
+  puts " #{board[1][0]} | #{board[1][1]} | #{board[1][2]} "
+  puts "-----------"
+  puts " #{board[2][0]} | #{board[2][1]} | #{board[2][2]} "
+end
+
+visualize_board(board)
