29ce0b18bd8a1e3cae98dfde7207dfbed5297166
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-07-30 15:16:29 +0100

    Fix #create_player test by stubbing puts

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,6 +7,7 @@ describe Player do
       allow(Player).to receive(:increase_total_number_players_by_one).and_return(1)
       allow(Player).to receive(:gets).and_return("Oscar\n")
       allow(Player).to receive(:assign_player_token).and_return("X")
+      allow(Player).to receive(:puts)
 
       expect(Player).to receive(:new).with("Oscar", "X", 1)
       Player.create_player
@@ -18,4 +19,37 @@ describe Player do
       expect { Player.increase_total_number_players_by_one }.to change { Player.total_number_players }.by(1)
     end
   end
+=begin
+  describe "#assign_player_token" do
+    context "when player 1 choses X" do
+      before do
+        allow(Player).to receive(:request_player_token_choice).and_return("X")
+        allow(Player).to receive(:update_remaining_tokens)
+      end
+
+      context "when no token was assigned" do
+        it "it returns player 1's token choice" do
+          Player.total_number_players = 1 #####?
+          expect(Player.assign_player_token).to eq("X")
+        end
+      end
+
+      context "when a token was already assigned to player 1" do
+        xit "it assigns remaining token to player 2" do
+          expect(Player.assign_player_token).to eq("O")
+        end
+      end
+    end
+
+
+
+    context "when a token was already assigned to player 1" do
+      xit "it assigns remaining token to player 2" do
+      end
+    end
+  end
+
+  ## request_player_token_choice
+  ## 
+=end
 end
