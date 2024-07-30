f914ca6c8ec50279d6ce73f7e9d53e083cdb6de0
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-07-30 15:51:23 +0100

    Create test for  #assign_player_token

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,12 +15,14 @@ describe Player do
   end
 
   describe "#increase_total_number_players_by_one" do
+  # This is a public class method that functions as Command method -> Test the change of state
     it "increases @@total_number_players by 1" do
       expect { Player.increase_total_number_players_by_one }.to change { Player.total_number_players }.by(1)
     end
   end
-=begin
+
   describe "#assign_player_token" do
+    # Query method --> Test the return value
     context "when player 1 choses X" do
       before do
         allow(Player).to receive(:request_player_token_choice).and_return("X")
@@ -29,27 +31,38 @@ describe Player do
 
       context "when no token was assigned" do
         it "it returns player 1's token choice" do
-          Player.total_number_players = 1 #####?
           expect(Player.assign_player_token).to eq("X")
         end
       end
 
       context "when a token was already assigned to player 1" do
-        xit "it assigns remaining token to player 2" do
+        it "it assigns remaining token to player 2" do
+          Player.class_variable_set(:@@total_number_players, 2)
+          Player.class_variable_set(:@@remaining_token_after_first_players_choice, 'O')
           expect(Player.assign_player_token).to eq("O")
         end
       end
     end
 
+    context "when player 1 choses O" do
+      before do
+        allow(Player).to receive(:request_player_token_choice).and_return("O")
+        allow(Player).to receive(:update_remaining_tokens)
+      end
 
+      context "when no token was assigned" do
+        it "it returns player 1's token choice" do
+          expect(Player.assign_player_token).to eq("O")
+        end
+      end
 
-    context "when a token was already assigned to player 1" do
-      xit "it assigns remaining token to player 2" do
+      context "when a token was already assigned to player 1" do
+        it "it assigns remaining token to player 2" do
+          Player.class_variable_set(:@@total_number_players, 2)
+          Player.class_variable_set(:@@remaining_token_after_first_players_choice, 'X')
+          expect(Player.assign_player_token).to eq("X")
+        end
       end
     end
   end
-
-  ## request_player_token_choice
-  ## 
-=end
 end
